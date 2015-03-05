40e29feabc75045c0039424afcae57a36be25920
Benjamin Watts <bwatts2@uccs.edu>
2015-03-05 15:19:13 -0400

    fixed the initial page view by changing routes.rb

--- WebComic_Rating_Service/config/routes.rb
+++ WebComic_Rating_Service/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :comics
+  root :to => redirect('/comics')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
