30ff909b79d8c7803f1f812958c20034299ff384
Sawyer Peterson <fleo_peterson@yahoo.com>
2015-03-06 15:20:08 -0400

    Seeded the database and added a few tests

--- WebComic_Rating_Service/db/seeds.rb
+++ WebComic_Rating_Service/db/seeds.rb
@@ -1,7 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Comics added from Piaza discussion. Feel free to add more if you want
+
+#Add Homestuck
+comicsList = [{:name => "Homestuck", :author => "Andrew Hussie", :url => "http://www.mspaintadventures.com/?s=6", :synopsis => "A young boy plays a video game with some of his friends and learns that this game will decide the fate of not only his own universe, but perhaps all of reality", :genre => "fantasy"}]
+#Add Paranatural
+comicsList << {:name => "Paranatural", :author => "Zack Morrison", :url => "http://www.paranatural.net/index.php?id=1", :synopsis => "Max is a kid who just moved into a new town. When he arives at his new school he is quickly dragged into the supernatural world of fighting evil spirits", :genre => "supernatural"}
+#Add Ava's Demon
+comicsList << {:name => "Ava's Demon", :author => "Michelle Czajkowski", :url => "http://www.avasdemon.com/", :synopsis => "A girl 1000 years in the future is plagued with a demon with alterior motives", :genre => "supernatural, scifi"}
+#Add Monster Pulse
+comicsList << {:name => "Monster Pulse", :author => "Magnolia Porter", :url => "http://www.monster-pulse.com/index.php?id=2", :synopsis => "Bina Blum gets wrapped up in a world of secret organisations and spirits when a strange creature jumps through her body and causes her heart to become its own sentient creature", :genre => "supernatural"}
+#Add Problem Sleuth
+comicsList << [{:name => "Problem Sleuth", :author => "Andrew Hussie", :url => "http://www.mspaintadventures.com/?s=4", :synopsis => "Three detectives try to escape their office building, which they have all managed to be trapped in. Shenanigans ensue", :genre => "I don't even know how to catagorize this mess"}]
+
+#Add comics to database
+comicsList.each do |comic|
+	Comic.create!(comic)
+end
--- WebComic_Rating_Service/features/home_page.feature
+++ WebComic_Rating_Service/features/home_page.feature
@@ -3,12 +3,26 @@ Feature: Add a comic to the list
     I want to be able to add my comic to the website
     So others can see it and review it
 
-    Scenario: Add a comic named "Insert real comic name here"
+    Scenario: Add a new comic (happy path)
  	    When I click "New Comic"
  	    Then I should see "New comic" 
- 	    When I fill in "Name" with "Insert real comic name here"
+ 	    When I fill in "Name" with "Paradox Space"
  	    And I click "Create Comic"
  	    Then I should see "Comic was successfully created."
  	    And I click "Back"
- 	    Then I should see "Insert real comic name here"
+ 	    Then I should see "Paradox Space"
+
+    Scenario: Try to add an existing comic (sad path)
+ 	    Given that there is a comic called "Homestuck"
+	    When I click "New Comic"
+ 	    Then I should see "New comic" 
+ 	    When I fill in "Name" with "Homestuck"
+ 	    And I click "Create Comic"
+ 	    Then I should see "That comic already exists"
+
+    Scenario: Try to add a null comic (sad path)
+ 	    When I click "New Comic"
+ 	    Then I should see "New comic" 
+ 	    And I click "Create Comic"
+ 	    Then I should see "You need to at least enter a comic name"
 
