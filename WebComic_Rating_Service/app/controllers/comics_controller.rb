568aea965f5d7706e5f0bf904932b69be284fe3b
Alex Indge <woopsire@comcast.net>
2015-03-12 18:42:37 -0600

    Implemented error checking for edit page as well.

--- WebComic_Rating_Service/app/controllers/comics_controller.rb
+++ WebComic_Rating_Service/app/controllers/comics_controller.rb
@@ -72,13 +72,36 @@ class ComicsController < ApplicationController
   # PATCH/PUT /comics/1.json
   def update
     respond_to do |format|
-      if @comic.update(comic_params)
-        format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully updated." }
-        format.json { render :show, status: :ok, location: @comic }
-      else
-        format.html { render :edit }
-        format.json { render json: @comic.errors, status: :unprocessable_entity }
-      end
+	if /http:\/\/..*[.]..*/.match comic_params[:url]
+	  if comic_params[:name].empty? 
+              format.html { redirect_to edit_comic_path(@comic), alert: "Required: Comic Name"}
+          else 
+              if @comic.update(comic_params)
+                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully changed." }
+                format.json { render :show, status: :ok, location: @comic }
+              else
+                format.html { render edit_comic_path(@comic) }
+                format.json { render json: @comic.errors, status: :unprocessable_entity }
+              end
+          end
+        else
+          if comic_params[:name].empty? 
+              format.html { redirect_to edit_comic_path(@comic), alert: "Required: Comic Name"}
+          else   
+	  if /..*[.]..*/.match comic_params[:url]
+	      @comic.url = "http://" + @comic.url
+	      if @comic.update(comic_params)
+                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully changed." }
+                format.json { render :show, status: :ok, location: @comic }
+              else
+                format.html { render edit_comic_path(@comic) }
+                format.json { render json: @comic.errors, status: :unprocessable_entity }
+              end
+  	  else
+	      format.html { redirect_to edit_comic_path(@comic) , alert: "Invalid URL. Example: http://www.example.com"}
+          end 
+	  end
+        end
     end
   end
 
@@ -100,6 +123,6 @@ class ComicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comic_params
-      params.require(:comic).permit(:name, :author, :url, :synopsis, :genre, :rating_art, :rating_story, :rating_overall)
+      params.require(:comic).permit(:name, :author, :url, :synopsis, :genre, :rating_art, :rating_story, :rating_overall, :update_schedule, :page_layout)
     end
 end
