19f3564a86bf3fe661e02251254cf82ca4f24b3e
Benjamin Watts <bwatts2@uccs.edu>
2015-04-02 12:27:16 -0600

    Removed user folder

--- WebComic_Rating_Service/db/schema.rb
+++ WebComic_Rating_Service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402174942) do
+ActiveRecord::Schema.define(version: 20150402182555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,6 @@ ActiveRecord::Schema.define(version: 20150402174942) do
     t.datetime "updated_at"
     t.string   "update_schedule"
     t.string   "page_layout"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
   end
 
 end
