3d0b3a6b0bd4401360f67d85f065b4f71a94b477
Alex Indge <woopsire@comcast.net>
2015-04-02 12:51:38 -0600

    User db doesn't break anymore.

--- WebComic_Rating_Service/config/routes.rb
+++ WebComic_Rating_Service/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comics
   root :to => redirect('/comics')
 
--- WebComic_Rating_Service/db/schema.rb
+++ WebComic_Rating_Service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402182555) do
+ActiveRecord::Schema.define(version: 20150402183407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,5 +30,30 @@ ActiveRecord::Schema.define(version: 20150402182555) do
     t.string   "update_schedule"
     t.string   "page_layout"
   end
+
+  create_table "helps", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
