c6ef52e460121d1b41747e71d3c6af57e08364a4
Sawyer Peterson <fleo_peterson@yahoo.com>
2015-04-20 16:00:47 -0300

    Refactored create and update

--- WebComic_Rating_Service/app/controllers/comics_controller.rb
+++ WebComic_Rating_Service/app/controllers/comics_controller.rb
@@ -20,13 +20,16 @@ class ComicsController < ApplicationController
   def reviseRate
     @comic = Comic.find(params[:id])
     ratingsHash = params[:ratings]
+    
     rates = checkNum(@comic.rates) + 1
     artVal = checkNum(@comic.rating_art)
     storyVal = checkNum(@comic.rating_story)
     overallVal = checkNum(@comic.rating_overall)
+
     art = ((artVal * (rates - 1)) + ratingsHash[:rating_art].to_i) / rates
     story = ((storyVal * (rates - 1)) + ratingsHash[:rating_story].to_i) / rates
     overall = ((overallVal * (rates - 1)) + ratingsHash[:rating_overall].to_i) / rates
+
     @comic.update(:rates => rates, :rating_art => art, :rating_story => story, :rating_overall => overall)
     respond_to do |format|    
 	format.html { redirect_to '/', notice: "#{@comic.name} rated"}
@@ -60,75 +63,46 @@ class ComicsController < ApplicationController
     respond_to do |format|
 	if Comic.pluck(:name).include? @comic.name
 	    format.html { redirect_to '/comics/new', alert: "Error: Comic \"#{@comic.name}\" Already Exists"} 
-        else
-	if /http:\/\/..*[.]..*/.match @comic.url
-	  if @comic.name.empty? 
-              format.html { redirect_to '/comics/new', alert: "Required: Comic Name"}
-          else 
-              if @comic.save
-                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully created." }
-                format.json { render :show, status: :created, location: @comic }
-              else
-                format.html { render :new }
-                format.json { render json: @comic.errors, status: :unprocessable_entity }
-              end
-          end
-        else
-          if @comic.name.empty? 
-              format.html { redirect_to '/comics/new', alert: "Required: Comic Name"}
-          else   
-	  if /..*[.]..*/.match @comic.url
-	      @comic.url = "http://" + @comic.url
-	      if @comic.save
-                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully created." }
-                format.json { render :show, status: :created, location: @comic }
-              else
-                format.html { render :new }
-                format.json { render json: @comic.errors, status: :unprocessable_entity }
-              end
-  	  else
-	      format.html { redirect_to '/comics/new', alert: "Invalid URL. Example: http://www.example.com"}
-          end 
-	  end
         end
+
+	if /http:\/\/..*[.]..*/.match @comic.url or /..*[.]..*/.match @comic.url
+  	else
+	  format.html { redirect_to '/comics/new', alert: "Invalid URL. Example: http://www.example.com"}
+        end 
+
+	if @comic.name.empty? 
+              format.html { redirect_to '/comics/new', alert: "Required: Comic Name"}
+        end
+	   
+        if @comic.save
+          format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully created." }
+          format.json { render :show, status: :created, location: @comic }
+        else
+          format.html { render :new }
+          format.json { render json: @comic.errors, status: :unprocessable_entity }
         end
     end
-    
   end
 
   # PATCH/PUT /comics/1
   # PATCH/PUT /comics/1.json
   def update
     respond_to do |format|
-	if /http:\/\/..*[.]..*/.match comic_params[:url]
-	  if comic_params[:name].empty? 
-              format.html { redirect_to edit_comic_path(@comic), alert: "Required: Comic Name"}
-          else 
-              if @comic.update(comic_params)
-                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully changed." }
-                format.json { render :show, status: :ok, location: @comic }
-              else
-                format.html { render edit_comic_path(@comic) }
-                format.json { render json: @comic.errors, status: :unprocessable_entity }
-              end
-          end
+	if /http:\/\/..*[.]..*/.match comic_params[:url] or /..*[.]..*/.match comic_params[:url]
+  	else 
+	 format.html { redirect_to edit_comic_path(@comic) , alert: "Invalid URL. Example: http://www.example.com"}
+        end 
+	
+	if comic_params[:name].empty? 
+          format.html { redirect_to edit_comic_path(@comic), alert: "Required: Comic Name"}
+        end
+
+	if @comic.update(comic_params)
+          format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully changed." }
+          format.json { render :show, status: :ok, location: @comic }
         else
-          if comic_params[:name].empty? 
-              format.html { redirect_to edit_comic_path(@comic), alert: "Required: Comic Name"}
-          else   
-	  if /..*[.]..*/.match comic_params[:url]
-	      @comic.url = "http://" + @comic.url
-	      if @comic.update(comic_params)
-                format.html { redirect_to @comic, notice: "Comic \"#{@comic.name}\" was successfully changed." }
-                format.json { render :show, status: :ok, location: @comic }
-              else
-                format.html { render edit_comic_path(@comic) }
-                format.json { render json: @comic.errors, status: :unprocessable_entity }
-              end
-  	  else
-	      format.html { redirect_to edit_comic_path(@comic) , alert: "Invalid URL. Example: http://www.example.com"}
-          end 
-	  end
+          format.html { render edit_comic_path(@comic) }
+          format.json { render json: @comic.errors, status: :unprocessable_entity }
         end
     end
   end
