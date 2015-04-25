3522d2e8e3066eb1bf4a7204a2fcbd7f5f533523
Sawyer Peterson <fleo_peterson@yahoo.com>
2015-04-25 00:42:01 -0300

    Iteration4-1 submission

--- WebComic_Rating_Service/db/migrate/20150305005249_create_comics.rb
+++ WebComic_Rating_Service/db/migrate/20150305005249_create_comics.rb
@@ -6,10 +6,10 @@ class CreateComics < ActiveRecord::Migration
       t.string :url
       t.string :synopsis
       t.string :genre
-      t.float :rating_art {:default=>0}
-      t.float :rating_story {:default=>0}
-      t.float :rating_overall {:default=>0}
-      t.integer :rates {:default=>0}
+      t.float :rating_art, {:default=>0}
+      t.float :rating_story, {:default=>0}
+      t.float :rating_overall, {:default=>0}
+      t.integer :rates, {:default=>0}
 
       t.timestamps
     end
